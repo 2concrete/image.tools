871c2aef32461ec55927aa0ef2df0355d79760f0
2concrete <danielpeace654@gmail.com>
2025-09-04 14:18:25 +0800

    added unselect functionality

--- src/components/ToolsButton.jsx
+++ src/components/ToolsButton.jsx
@@ -1,15 +1,17 @@
 const ToolsButton = ({ name, setCurrentEffect, currentEffect }) => {
-  console.log(
-    `button: ${name}, current: ${currentEffect}, match? ${
-      name === currentEffect
-    }`
-  );
+  const handleClick = () => {
+    if (currentEffect === name) {
+      setCurrentEffect("");
+    } else {
+      setCurrentEffect(name);
+    }
+  };
   return (
     <button
       className={`border-1 border-b-0 p-2 hover:bg-neutral-50  transition-all cursor-pointer ${
         currentEffect === name && "font-bold"
       }`}
-      onClick={() => setCurrentEffect(name)}
+      onClick={() => handleClick()}
     >
       {name}
     </button>
